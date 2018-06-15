aff4114d98b8dc143f4fc18b110e0cb70ba271f5
ismail moussaoui <ismailmoussaoui1@gmail.com>
2018-06-15 14:29:19 +0200

    fixing port

--- server/server.js
+++ server/server.js
@@ -5,6 +5,6 @@ const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 let app=express();
 app.use(express.static(publicPath));
-app.listen(3000, () => {
-  console.log(`Server is up on port {port}`)
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`)
 });
